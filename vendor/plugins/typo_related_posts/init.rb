601ff61c66bff2a1d4c2c3b341b1749552e09823
Harry Seldon <harry.seldon.f@gmail.com>
2008-11-03 22:32:39 +0100

    remove unnecessary plugin

--- vendor/plugins/typo_related_posts/init.rb
+++
@@ -1,6 +0,0 @@
-# Copyright 2008 Frédéric de Villamil
-
-require 'typo_related_posts'
-
-ActionController::Base.send :include, Typorelatedposts
-ActionController::Base.send :helper, Typorelatedpostshelper
